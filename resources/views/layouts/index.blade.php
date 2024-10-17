31e23f17d932dce9f5a957a30c9235467b3ade8b
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2024-10-17 14:22:19 +0700

    Update index.blade.php: Update about section content and add SchoolTech Indonesia description

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -56,39 +56,40 @@
         </div>
       </div>
 
-    </section><!-- /Hero Section -->
+    </section>
+    <!-- /Hero Section -->
 
     <!-- About Section -->
-{{--    <section id="about" class="about section">--}}
+   <section id="about" class="about section">
 
-{{--      <div class="container" data-aos="fade-up">--}}
-{{--        <div class="row gx-0">--}}
+     <div class="container" data-aos="fade-up">
+       <div class="row gx-0">
 
-{{--          <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">--}}
-{{--            <div class="content">--}}
-{{--              <h3>Who We Are</h3>--}}
-{{--              <h2>Expedita voluptas omnis cupiditate totam eveniet nobis sint iste. Dolores est repellat corrupti reprehenderit.</h2>--}}
-{{--              <p>--}}
-{{--                Quisquam vel ut sint cum eos hic dolores aperiam. Sed deserunt et. Inventore et et dolor consequatur itaque ut voluptate sed et. Magnam nam ipsum tenetur suscipit voluptatum nam et est corrupti.--}}
-{{--              </p>--}}
-{{--              <div class="text-center text-lg-start">--}}
-{{--                <a href="#" class="btn-read-more d-inline-flex align-items-center justify-content-center align-self-center">--}}
-{{--                  <span>Read More</span>--}}
-{{--                  <i class="bi bi-arrow-right"></i>--}}
-{{--                </a>--}}
-{{--              </div>--}}
-{{--            </div>--}}
-{{--          </div>--}}
+         <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
+           <div class="content">
+             <h3>Who We Are</h3>
+             <h2>SchoolTech Indonesia berkomitmen untuk memberikan kemudahan sekolah untuk melakukan proses digitalisasi dalam manajemen operasional.</h2>
+             <p>
+               Dengan dukungan teknologi yang sesuai, sekolah dapat mengurangi beban administratif dan fokus pada pendidikan. Kami menyediakan berbagai layanan yang dapat membantu sekolah dalam mengelola data, informasi, dan proses operasional.
+             </p>
+             <div class="text-center text-lg-start">
+               <a href="#" class="btn-read-more d-inline-flex align-items-center justify-content-center align-self-center">
+                 <span>Read More</span>
+                 <i class="bi bi-arrow-right"></i>
+               </a>
+             </div>
+           </div>
+         </div>
 
-{{--          <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">--}}
-{{--            <img src="assets/img/about.jpg" class="img-fluid" alt="">--}}
-{{--          </div>--}}
+         <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
+           <img src="assets/img/about.jpg" class="img-fluid" alt="">
+         </div>
 
-{{--        </div>--}}
-{{--      </div>--}}
+       </div>
+     </div>
 
-{{--    </section>
-<!-- /About Section -->--}}
+   </section>
+<!-- /About Section -->
 
     <!-- Values Section -->
     <section id="values" class="values section">
